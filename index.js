e87405d959e7c24007234ec1a4b483f715fe1f7a
skvinay <sk.vinay.38@gmail.com>
2023-09-05 21:27:08 +0530

    JS file added

---
+++ index.js
@@ -0,0 +1,16 @@
+let about = `Manbru Associates is a leading firm that specializes in providing comprehensive legal, 
+Chartered Accountant (CA), and Company Secretary (CS) services under one roof. With a team
+of highly skilled professionals, including experienced lawyers, certified accountants, and 
+company secretaries, we offer integrated solutions to meet the diverse needs of our clients. 
+Our firm is committed to delivering exceptional services that are tailored to the unique requirements
+of each client, whether they are individuals, small businesses, or large corporations. We combine our 
+legal expertise with financial acumen and corporate governance knowledge to provide comprehensive solutions
+for legal compliance, taxation matters, company formations, regulatory filings, and more. At Manbru Associates, 
+we strive to be a trusted partner for our clients, ensuring their legal, financial, and regulatory needs are met
+efficiently and effectively.`
+
+let aboutEle = document.getElementById('about')
+
+function aboutUs(){
+    aboutEle.textContent = about
+}
